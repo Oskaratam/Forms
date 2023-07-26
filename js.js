bf2582d7c3d19471db8a28000610fcc215e24a41
Oskaratam <oskaratamanenko2@gmail.com>
2023-07-27 00:00:46 +0300

    created a validation for the form, added visuals

--- js.js
+++ js.js
@@ -5,8 +5,13 @@ const container = document.querySelector('.container'),
       signUpForm = document.getElementById("signUp"),
       showPasswordBtn = document.getElementById("showPasswordBtn"),
       showPasswordBtn2 = document.getElementById("showPasswordBtn2"),
-      password = document.getElementById("password")
-      password2 = document.getElementById("password2");
+      password = document.getElementById("password"),
+      name2 = document.getElementById("name2"),
+      password2 = document.getElementById("password2"),
+      nameError = document.getElementById('nameError'),
+      passwordError = document.getElementById("passwordError");
+
+
 
 changeBtn.innerText = 'Create account';
 changeBtn.addEventListener('click', () => {
@@ -32,3 +37,42 @@ if (passwordTyped.type == 'password'){
     passwordTyped.type = 'password'
 }
 }
+
+//validating form
+signUpForm.addEventListener('submit', (e) => {
+    let messageForName = [],
+    messageForPassword = [];
+    passwordError.innerHTML = null;
+    nameError.innerHTML = null;
+    name2.classList.remove('errorInput');
+    password2.classList.add('errorInput');
+
+
+    if (name2.value.length <= 3){
+        messageForName.push('Name length must be longer than 3 characters')
+    } else if (name2.value.length >= 15) {
+        messageForName.push('Name length must be shorter than 15 characters')
+    }
+
+    if (password2.value.length <= 5){
+        messageForPassword.push("Password can't be shorter than 5")
+    }else if (password2.value.length > 20){
+        messageForPassword.push("Password can't be longer than 20")
+    }
+     if (/[0-9]/.test(password.value)) {  //this validation doesn't work now
+        messageForPassword.push("")
+    }else {
+        messageForPassword.push("Password must contain numbers")
+    }
+
+    if (messageForName.length > 0) {
+        e.preventDefault();
+        nameError.append(messageForName.join(' | '));
+        name2.classList.add('errorInput');
+    }
+    if (messageForPassword.length > 0) {
+        e.preventDefault();
+        passwordError.append(messageForPassword.join(' | '));
+        password2.classList.add('errorInput');
+    }
+})
